91ca9919fdc572aebb2a56e7401bb397d0de718c
IlCarretto <carlorebuffat412@gmail.com>
2022-12-15 16:17:56 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<div class="band"></div>
+<div class="container">
+    <header class="header">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+        </div>
+        <nav class="header-nav">
+            <ul>
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="">COMICS</a>
+                </li>
+                <li>
+                    <a href="">MOVIES</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">GAMES</a>
+                </li>
+                <li>
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="">FANS</a>
+                </li>
+                <li>
+                    <a href="">NEWS</a>
+                </li>
+                <li>
+                    <a href="">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</div>
